d80f180928e586c448d2cf7c5737f299adad55fb
Lauren Colls <lauren.colls@hotmail.com>
2016-04-17 19:59:24 +1200

    clean code

--- js/calculator.js
+++ js/calculator.js
@@ -1,93 +1,58 @@
 
 var HTMLoutput = '<p>%data%</p>'
  
-
-
 var answer
 var buttValue
-var remValues = []
-var theOp
 var values = []
-var value1
-var value2
-
 
 $(document).ready(function () {
-	 $('button').click(function () {
-	 	 buttValue = $(this).attr('value');
-	 	 // console.log(buttValue)
-     nextStep(buttValue)
-	 	 });
+	$('button').click(function () {
+		buttValue = $(this).attr('value');
+		nextStep(buttValue)
+	});
 
 });
 
 function nextStep(val){
-		if (val === "Cl" ) { 
-	 	 	clearAll()
-	 	 } else if (val === "=") {
-	 	 	calculate(valOut)
+	if (val === "Cl" ) { 
+ 	 	clearAll()
+ 	 } else if (val === "=") {
+ 	 	calculate(valOut)
 
-	 	 } else {
+ 	 } else {
 		updateScreen(val)
-		// console.log(val)
 
-		}
+	}
 
 }
 
 function clearAll(){
-values = []
-valOut = ""
+	values = []
+	valOut = ""
 
-updateScreen()
+	updateScreen()
 
 }
 
 
-// function assignValues(val){
-
-// 	if(isNaN(val) === false){
-// 		updateScreen(val)
-// 		values = values.push(val)
-// 		console.log(val)
-// 	} else if  (val === "Cl" ) {
-
-// 		val = ""
-// 		updateScreen(val)
-// 		console.log(val)
-// 	}
-
-// }
 
 function updateScreen (val) {
 
- // if (isNaN(val) === false) {
 	values.push(val)
 	valOut = values.join("")
-					console.log(values)
-					console.log(valOut)
+					
 	var formattedOutput = HTMLoutput.replace("%data%", valOut) || function(){};
 	$(".output").html(formattedOutput)
 
 	
-	 // } else if (isNaN(val) === true) {
-
- // 	 theOp = val
- // 	 var formattedOutput = HTMLoutput.replace("%data%", theOp) || function(){};
-	// $(".output").html(formattedOutput)
-	// values = []
-
- // }
-
 }
 
 function calculate(str){
 	
 	answer = eval(str)
-
-console.log(answer)
-valOut = ""
-clearAll()
+	console.log(answer)
+	valOut = ""
+	clearAll()
 	updateScreen(answer)
 
 }
